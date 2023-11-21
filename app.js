584bccc478a2491ad51bfc15ae6711768b47dd52
James Dillard <jamesdillard@Jamess-MacBook-Air.local>
2023-11-21 14:39:40 -0500

    update

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav_menu");
 const galleryImage = document.querySelectorAll(".gallery_image");
+let imgIndex;
+let imgSrc;
 
-galleryImage.forEach((img) => {
+galleryImage.forEach((img, i) => {
   img.addEventListener("click", (e) => {
-    modal(e.target.src);
+    imgSrc = e.target.src;
+    imgIndex = i;
+    modal(imgSrc, imgIndex);
   });
 });
 
@@ -13,16 +17,49 @@ hamburger.addEventListener("click", function () {
   navMenu.classList.toggle("active");
 });
 
-function modal(src) {
+function modal(src, imgIndex) {
+  //create modal
   const modal = document.createElement("div");
+  modal.setAttribute("class", "modal");
+  document.querySelector(".gallery_main").append(modal);
+  //create image
   const newImage = document.createElement("img");
+  newImage.setAttribute("src", src);
+
+  //add close button
   const closeBtn = document.createElement("i");
   closeBtn.setAttribute("class", "fa-solid fa-xmark closeBtn");
   closeBtn.addEventListener("click", () => {
     modal.remove();
   });
-  modal.setAttribute("class", "modal");
-  document.querySelector(".gallery_main").append(modal);
-  newImage.setAttribute("src", src);
-  modal.append(newImage, closeBtn);
+  //add next button
+  const nextBtn = document.createElement("i");
+  nextBtn.setAttribute("class", "fa-solid fa-chevron-right nextBtn");
+  nextBtn.addEventListener("click", () => {
+    newImage.setAttribute("src", nextImage());
+  });
+  // add prev button
+  const prevBtn = document.createElement("i");
+  prevBtn.setAttribute("class", "fa-solid fa-chevron-left prevBtn");
+  prevBtn.addEventListener("click", () => {
+    newImage.setAttribute("src", prevImage());
+  });
+
+  modal.append(newImage, closeBtn, prevBtn, nextBtn);
+}
+
+function nextImage() {
+  imgIndex++;
+  if (imgIndex >= galleryImage.length) {
+    imgIndex = 0;
+  }
+  return galleryImage[imgIndex].src;
+}
+
+function prevImage() {
+  imgIndex--;
+  if (imgIndex < 0) {
+    imgIndex = galleryImage.length - 1;
+  }
+  return galleryImage[imgIndex].src;
 }
