89368ec6c53a6da940862c0bd010bb1bea5111ce
James Dillard <jamesdillard@Jamess-MacBook-Air.local>
2023-11-20 22:25:38 -0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav_menu");
+const galleryImage = document.querySelectorAll(".gallery_image");
+
+galleryImage.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    modal(e.target.src);
+  });
+});
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+function modal(src) {
+  const modal = document.createElement("div");
+  const newImage = document.createElement("img");
+  const closeBtn = document.createElement("i");
+  closeBtn.setAttribute("class", "fa-solid fa-xmark closeBtn");
+  closeBtn.addEventListener("click", () => {
+    modal.remove();
+  });
+  modal.setAttribute("class", "modal");
+  document.querySelector(".gallery_main").append(modal);
+  newImage.setAttribute("src", src);
+  modal.append(newImage, closeBtn);
+}
